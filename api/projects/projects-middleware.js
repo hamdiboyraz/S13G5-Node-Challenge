517156f96723a4cd24214ae0bda3164b70ebc418
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-21 06:00:25 +0300

    Add project routes and project middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,38 @@
 // projects ara yazılımları buraya
+const projectsController = require("./projects-model");
+
+function validateProject(req, res, next) {
+  try {
+    // console.log(req.body);
+    const { name, description, completed } = req.body;
+    if (!name || !description) {
+      return res.status(400).json({
+        message: "Please fill in all fields.",
+      });
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+async function validateProjectID(req, res, next) {
+  try {
+    const { id } = req.params;
+    const project = await projectsController.get(id);
+    if (!project) {
+      return res.status(404).json({ message: "Project not found!" });
+    }
+    req.project = project;
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+module.exports = {
+  validateProject,
+  validateProjectID,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,8 +1,110 @@
 // "project" routerını buraya yazın!
 const express = require("express");
 const projectsController = require("./projects-model");
+const projectsMiddleware = require("./projects-middleware");
 
 // Initialize a router
 const projectsRouter = express.Router();
+
+// Routes for the projects API -> /api/projects
+// Get All Projects
+projectsRouter.get("/", async (req, res, next) => {
+  try {
+    const projects = await projectsController.get();
+    if (!projects) {
+      return res.status(200).json([]);
+    }
+    return res.status(200).json(projects);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// Create Project
+projectsRouter.post(
+  "/",
+  projectsMiddleware.validateProject,
+  async (req, res, next) => {
+    try {
+      const { name, description } = req.body;
+      const newProject = await projectsController.insert({ name, description });
+      res.status(201).json(newProject);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+// Get Project
+projectsRouter.get(
+  "/:id",
+  projectsMiddleware.validateProjectID,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const project = await projectsController.get(id);
+      res.status(200).json(project);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+// Update Project
+projectsRouter.put(
+  "/:id",
+  projectsMiddleware.validateProject,
+  projectsMiddleware.validateProjectID,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { name, description, completed } = req.body;
+      const updatedProject = await projectsController.update(id, {
+        name,
+        description,
+        completed,
+      });
+      res.status(200).json(updatedProject);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+// Delete Project
+projectsRouter.delete(
+  "/:id",
+  projectsMiddleware.validateProjectID,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await projectsController.remove(id);
+      res.status(204).send();
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+// Get All Actions for Project
+projectsRouter.get(
+  "/:id/actions",
+  projectsMiddleware.validateProjectID,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const projectActions = await projectsController.getProjectActions(id);
+      res.status(200).json(projectActions);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
 
 module.exports = projectsRouter;
